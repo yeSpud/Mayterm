8a0df6e3f20f2dc5b5fd6223d90c38dd7826b30a
Stephen <jeffrypig23@gmail.com>
2018-05-13 20:26:44 -0700

    BPM count

--- src/application/Genre.java
+++ src/application/Genre.java
@@ -3,9 +3,18 @@ package application;
 import javafx.scene.paint.Color;
 
 public enum Genre {
-	TRAP(140, 15, 39), DNB(242, 25, 4), HOUSE(234, 140, 0), ELECTRO(230, 206, 0), HARDDANCE(1, 151, 0), GLITCHHOP(11,
-			151, 87), NUDISCO(28, 171, 179), FUTUREBASS(154, 152,
-					252), TRANCE(0, 126, 231), DUBSTEP(141, 4, 225), DRUMBSTEP(243, 33, 136), ELECTRONIC(193, 193, 193);
+	TRAP(140, 15, 39), // Trap: 75 - 175 bpm
+	DNB(242, 25, 4), // DnB: 160 - 180 or 87 bpm
+	HOUSE(234, 140, 0), // House: ~128 bpm
+	ELECTRO(230, 206, 0), // Electro: ~128 bpm
+	HARDDANCE(1, 151, 0), // Harddance: 150 or 170 bpm
+	GLITCHHOP(11, 151, 87), // Glitch: 110 or 108 bpm
+	NUDISCO(28, 171, 179), // NuDisco: 115 - 126 bpm
+	FUTUREBASS(154, 152, 252), // Future bass: 75 - 175 bpm
+	TRANCE(0, 126, 231), // Trance: 125 (135) - 150 bpm
+	DUBSTEP(141, 4, 225), // Dubstep: 70 bpm or 140 - 150 bpm
+	DRUMSTEP(243, 33, 136), // Drumstep: 160 - 180 bpm
+	ELECTRONIC(193, 193, 193); // Catch all
 
 	private final int r, g, b;
 
