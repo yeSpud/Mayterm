1d24d46a02de8b08c975b4d0dc199f4a4cbe5fb7
Spud <jeffrypig23@gmail.com>
2018-05-16 17:29:36 -0700

    Found new test code! Time to see how it handles!

---
+++ src/application/Tests/spectrum.java
@@ -0,0 +1,10 @@
+package application.Tests;
+
+public class spectrum {
+
+	public static void foo() {
+		double initialArray[] = new double[63];
+		spectrum_algorithms.transformToVisualBins(initialArray);
+	}
+	
+}
