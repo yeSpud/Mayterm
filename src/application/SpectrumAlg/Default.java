6e3f3519aa246102ed41a012c441182d8ef6f945
Spud <jeffrypig23@gmail.com>
2018-08-05 20:49:02 +0800

    Added a check to see if the values changed or not
    Basically I wanted to try to remove some of the lag that may be caused by updating the length of the bars by removing the need to update the value if its the same. So it now checks if the value has changed and then updates it.

--- src/application/SpectrumAlg/Default.java
+++ src/application/SpectrumAlg/Default.java
@@ -10,8 +10,12 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
 /**
- * The original spectrum algorithm.<br ><br >Yeah it's really simple, but its not accurate. That being said, if the
- * official one never gets finished, there's always this {@code Fallback}.<br ><br ><i>See what I did there? Look I'm funny.</i>
+ * The original spectrum algorithm.<br >
+ * <br >
+ * Yeah it's really simple, but its not accurate. That being said, if the
+ * official one never gets finished, there's always this {@code Fallback}.<br >
+ * <br >
+ * <i>See what I did there? Look I'm funny.</i>
  * 
  * @author Spud
  *
@@ -32,14 +36,17 @@ public class Default implements AudioSpectrumListener {
 
 		for (int i = 0; i < magnitudes.length; i++) {
 			Rectangle bar = (Rectangle) Spectrum.spectrum.getChildren().get(62 - i);
-			bar.setHeight((63 - magnitudes[i] * -1) * 4);
-			try {
-				Text text = (Text) SpectrumDebug.spectrumText.getChildren().get(i);
-				text.setText(String.valueOf(String.format("%.2f", bar.getHeight())));
-				text.setX(bar.getX());
-				text.setY(bar.getY() - 15);
-			} catch (IndexOutOfBoundsException foo) {
-				// Do nothing
+			float height = ((63 - magnitudes[i] * -1) * 4);
+			if (height != bar.getHeight()) {
+				bar.setHeight(height);
+				try {
+					Text text = (Text) SpectrumDebug.spectrumText.getChildren().get(i);
+					text.setText(String.valueOf(String.format("%.2f", bar.getHeight())));
+					text.setX(bar.getX());
+					text.setY(bar.getY() - 15);
+				} catch (IndexOutOfBoundsException foo) {
+					// Do nothing
+				}
 			}
 		}
 
