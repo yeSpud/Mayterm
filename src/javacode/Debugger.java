7137ecfbf2db93ca26c79c0d7d3df4fd6cef47d8
Spud <jeffrypig23@gmail.com>
2018-11-14 21:58:40 -0800

    Trying to add an error screenish type deal

--- src/javacode/Debugger.java
+++ src/javacode/Debugger.java
@@ -1,5 +1,11 @@
 package javacode;
 
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
 public class Debugger {
 
 	Main main = new Main();
@@ -11,6 +17,23 @@ public class Debugger {
 		} else {
 			return;
 		}
+	}
+	
+	public void e(Class<?> c, String error, String trace, StackPane stack) {
+		String errorHeader = String.format("%s: %s",c.getName(), error);
+		
+		System.out.println(errorHeader);
+		
+		Text errorHUD = new Text(String.format("%s\n%s", errorHeader, trace));
+		
+		errorHUD.setFont(new Font("Arial", 15));
+		errorHUD.setWrappingWidth(500);
+		errorHUD.setFill(Color.RED);
+		errorHUD.setTranslateX(0);
+		
+		
+		stack.getChildren().add(0, errorHUD);
+		
 	}
 	
 }
--- src/javacode/Main.java
+++ src/javacode/Main.java
@@ -14,16 +14,19 @@ import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
 	public boolean debug = false;
+	
+	StackPane stack;
 
 	@Override
 	public void start(Stage stage) {
 
-		
 		// Check for updates
 		System.out.println("Checking for update");
 		boolean newUpdate = Updater.updateAvalible();
@@ -31,66 +34,69 @@ public class Main extends Application {
 		if (newUpdate) {
 			Updater.showUpdatePrompt();
 		}
-		
-		
+
 		// Get the debug background image
-		Image debugbackgroundimage = new Image(this.getClass().getClassLoader().getResourceAsStream("resources/debugbackground.jpg"));
-		
+		Image debugbackgroundimage = new Image(
+				this.getClass().getClassLoader().getResourceAsStream("resources/debugbackground.jpg"));
+
 		// Create a stack pane to add all the objects into
-		StackPane stack = new StackPane();
-		
-		stack.setBackground(new Background(new BackgroundImage(debugbackgroundimage, 
-				BackgroundRepeat.NO_REPEAT, 
-				BackgroundRepeat.NO_REPEAT, 
-				BackgroundPosition.DEFAULT, 
-				new BackgroundSize(debugbackgroundimage.getWidth(), debugbackgroundimage.getHeight(), false, false, true, true))));
-		
+		stack = new StackPane();
+
+		// Setup the background
+		stack.setBackground(new Background(new BackgroundImage(debugbackgroundimage, BackgroundRepeat.NO_REPEAT,
+				BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, new BackgroundSize(
+						debugbackgroundimage.getWidth(), debugbackgroundimage.getHeight(), false, false, true, true))));
+
+		// Set the preferred and minimum sizes for the stack
 		stack.setPrefSize(debugbackgroundimage.getWidth(), debugbackgroundimage.getHeight());
 		stack.setMinSize(16, 9);
-		
-		
-		// We need an anchor pane go help automatically constrain the maximum and minimum sizes of things
+
+		// We need an anchor pane go help automatically constrain the maximum and
+		// minimum sizes of things
 		AnchorPane anchor = new AnchorPane();
 		AnchorPane.setTopAnchor(stack, 0.0);
 		AnchorPane.setBottomAnchor(stack, 0.0);
 		AnchorPane.setLeftAnchor(stack, 0.0);
 		AnchorPane.setRightAnchor(stack, 0.0);
-		
-		
+
 		// Add the stack to the anchor pane
 		anchor.getChildren().addAll(stack);
-		
-		
+
 		// Set the anchor background to a light gray, that way we can check for overlap
 		anchor.setBackground(new Background(new BackgroundFill(Color.LIGHTGRAY, CornerRadii.EMPTY, Insets.EMPTY)));
-		
-		
-		
+
 		// Set the scene for the visualizer, use the anchor pane defined above
 		Scene mCatScene = new Scene(anchor);
-		
-		
+
 		// Set the contents of the window to that of the scene
 		stage.setScene(mCatScene);
-		
-		
+
 		// Show the window (stage)
 		stage.show();
+		
+		Text test = new Text(String.format("Test\nFoo bar"));
+		
+		test.setFont(new Font("Arial", 15));
+		test.setWrappingWidth(500);
+		test.setFill(Color.RED);
+		test.setX(0);
+		test.setY(0);
+		
+		stack.getChildren().add(0, test);
 
 	}
 
 	public static void main(String[] args) {
-		
+
 		Main This = new Main();
-		
+
 		// Check if debug mode is enabled (Basically check for console spam enabled)
 		try {
 			This.debug = Boolean.parseBoolean(args[0]);
 		} catch (Exception e) {
 			This.debug = false;
 		}
-		
-		
+
 		launch(args);
 
 	}
