f3f0070de03549e76cd0502e68690a24619a1b8a
Spud <jeffrypig23@gmail.com>
2018-11-15 09:30:08 -0800

    Tried moving things

--- src/javacode/Main.java
+++ src/javacode/Main.java
@@ -22,7 +22,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
 	public boolean debug = false;
-	
+
 	Pane stack;
 
 	@Override
@@ -60,7 +60,6 @@ public class Main extends Application {
 		AnchorPane.setLeftAnchor(stack, 0.0);
 		AnchorPane.setRightAnchor(stack, 0.0);
 
-
 		// Add the stack to the anchor pane
 		anchor.getChildren().addAll(stack);
 
@@ -73,27 +72,26 @@ public class Main extends Application {
 		// Set the contents of the window to that of the scene
 		stage.setScene(mCatScene);
 
-		// Show the window (stage)
-		stage.show();
-		
 		// Add a test node to check if its size and position updates
 		Text test = new Text(String.format("Test\nFoo bar"));
-		
-		test.setFont(new Font("Arial", 15));
+
+		// test.setFont(new Font("Arial", 15));
 		test.setWrappingWidth(500);
 		test.setFill(Color.RED);
 		test.setLayoutX(0);
 		test.setLayoutY(50);
-		
+
 		Circle test2 = new Circle();
 		test2.setLayoutX(50);
 		test2.setLayoutY(120);
 		test2.setFill(Color.GREEN);
 		test2.setRadius(10);
-		
-		
+
 		stack.getChildren().add(0, test);
 		stack.getChildren().add(1, test2);
+
+		// Show the window (stage)
+		stage.show();
 
 	}
 
